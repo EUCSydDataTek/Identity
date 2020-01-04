79fb508ee4be06e8740bbf060b4cd81fc96cce25
ECR\Egon Rasmussen <ecr@live.dk>
2020-01-04 11:05:58 +0100

    ok

--- WebAppLocalStorage/Pages/Privacy.cshtml.cs
+++ WebAppLocalStorage/Pages/Privacy.cshtml.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace WebAppLocalStorage.Pages
 {
+    [Authorize]
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
