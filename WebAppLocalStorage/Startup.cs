2c01a55dd471ecc22684c55ce2453558e8db3e92
ECR\EgonRasmussen <ecr@eucsyd.dk>
2022-02-05 16:13:42 +0100

    ok

--- WebAppLocalStorage/Startup.cs
+++ WebAppLocalStorage/Startup.cs
@@ -88,7 +88,6 @@ namespace WebAppLocalStorage
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
             }
             else
             {
